8a70b60f5062aa6b9b9bfcc15153ecac92b9f885
edangelo <eduardo@adgistics.com>
2019-07-19 17:09:10 +0100

    Updates

---
+++ precache-manifest.992bdf8c9c09116afdf06fb461044600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c356c7bb4602dc7f2f297a041613b5c9",
+    "url": "/fire-grid/index.html"
+  },
+  {
+    "revision": "ae493e6e90f9c9276f54",
+    "url": "/fire-grid/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/fire-grid/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ae493e6e90f9c9276f54",
+    "url": "/fire-grid/static/js/main.15ee5f02.chunk.js"
+  },
+  {
+    "revision": "ec407bad19de82e8375b",
+    "url": "/fire-grid/static/js/runtime~main.86ed94d9.js"
+  }
+]);
